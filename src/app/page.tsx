273337ea93b1751f118038d4d66c9971a2b9aae8
MS Kang <tomskang@naver.com>
2025-04-03 16:42:52 +0900

    fix: 메인 페이지 번경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="home-container">
       <div className="home-inner">
-        <h1 className="home-title">새내기 공학캠프 1회</h1>
-        <p className="home-subtitle">Pseudo Random Number Generator</p>
+        <h1 className="home-title">제1회 새내기 공학캠프</h1>
+        <p className="home-subtitle">경품 추첨</p>
         <div className="home-link-container">
           <Link href="/generate" className="home-link">
             Go to Generate
