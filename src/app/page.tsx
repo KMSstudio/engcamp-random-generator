29d147826988fb43d626d7b14f214a6e58b9daba
MS Kang <tomskang@naver.com>
2025-04-06 00:52:55 +0900

    fix: 메인 페이지에 사진 보이게 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="home-container">
       <div className="home-inner">
-        <h1 className="home-title">제1회 새내기 공학캠프</h1>
-        <p className="home-subtitle">경품 추첨</p>
+        <img src="/image/title.png" alt="Title" className="home-title-image" />
+        <h1 className="home-title">제1회 새내기 공학캠프 경품 추첨</h1>
         <div className="home-link-container">
           <Link href="/generate" className="home-link">
             Go to Generate
